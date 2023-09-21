9efbbea954116dad4f1598b93ed8ce5f52aa7b53
Trần Quang Kiệt <138895139+KietTranUIT@users.noreply.github.com>
2023-09-21 22:06:10 +0700

    feat(backend/main.go): create mul and devide two integers feature

--- backend/main.go
+++ backend/main.go
@@ -10,12 +10,20 @@ func sub(a, b int) int {
 return a - b
 }
 
+func mul(a, b int) int {
+return a * b
+}
+
+func divide(a, b int) int {
+return a / b
+}
+
 func main() {
 var a, b int
 
 fmt.Printf("Enter values for a and b: ")
 fmt.Scanf("%d %d", &a, &b)
 
-fmt.Printf("a + b = %d\n", add(a,b))
-fmt.Printf("a - b = %d\n", sub(a,b))
+fmt.Printf("a + b = %d\n", mul(a, b))
+fmt.Printf("a - b = %d\n", divide(a, b))
 }
