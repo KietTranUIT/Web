668f5145dc27de631006c890a8315d30ae7d00cb
Tran Kiet <kiettranuit@gmail.com>
2023-09-21 21:57:34 +0700

    feat(backend folder): create a program add and sub two integers

---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func add(a, b int) int {
+return a + b
+}
+
+func sub(a, b int) int {
+return a - b
+}
+
+func main() {
+var a, b int
+
+fmt.Printf("Enter values for a and b: ")
+fmt.Scanf("%d %d", &a, &b)
+
+fmt.Printf("a + b = %d\n", add(a,b))
+fmt.Printf("a - b = %d\n", sub(a,b))
+}
